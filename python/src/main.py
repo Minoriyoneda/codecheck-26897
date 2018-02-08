895f0dd61a594bf3a4b8ce204879e01cef38cc27
Minori Ricefield <minori.yoneda.0504@gmail.com>
2018-02-08 19:25:05 +0700

    Update main.py

--- python/src/main.py
+++ python/src/main.py
@@ -1,13 +1,11 @@
 import ys, os, datetime, openpyxl
-for line in sys.stdin:
-  print("0\n0\n0\n0\n0")
-  eachline = line[:-1].split(",")
-  date = eachline[0]
-  first = eachline[1]
-  second = eachline[2]
-  third = eachline[3]
+import linecache
+
+def main():
   
- 
+data = sys.stdin.readlines()
+
+
   
 
 
